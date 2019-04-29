eae85545e9681075f2dc372f1f041660fa54c0f0
dineshkuncham <dineshkuncham77@gmail.com>
2019-04-28 21:06:54 -0300

    added new publisher entity and linked with book entity using one-to-one relationship

--- src/main/java/com/springwebapp/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/springwebapp/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.springwebapp.spring5webapp.bootstrap;
 
 import com.springwebapp.spring5webapp.model.Author;
 import com.springwebapp.spring5webapp.model.Book;
+import com.springwebapp.spring5webapp.model.Publisher;
 import com.springwebapp.spring5webapp.repositories.AuthorRepository;
 import com.springwebapp.spring5webapp.repositories.BookRepository;
+import com.springwebapp.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
  private AuthorRepository authorRepository;
  private BookRepository bookRepository;
+ private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,18 +30,30 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
 
+        //publisher 1
+        Publisher dddPublisher = new Publisher();
+        dddPublisher.setName("kuncham");
+        dddPublisher.setAddress("Halifax, NS");
+        publisherRepository.save(dddPublisher);
+
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book  ddd = new Book("Domain Driven Design", "1234",dddPublisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
+        //publisher 2
+        Publisher noEJBPublisher = new Publisher();
+        noEJBPublisher.setName("dinesh");
+        noEJBPublisher.setAddress("Halifax, NS");
+        publisherRepository.save(noEJBPublisher);
+
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        Book noEJB = new Book("J2EE Development without EJB", "23444", noEJBPublisher );
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
